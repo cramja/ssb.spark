887afef041b04bc41329cef78a0e35f3466677fc
rogersjeffreyl <rl@cs.wisc.edu>
2016-05-03 02:57:19 -0500

    Adding code  for options

--- src/main/scala/SSB.scala
+++ src/main/scala/SSB.scala
@@ -6,6 +6,13 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.SQLContext._
 import java.io.File
 
+case class RunConfig(
+    ssb_path: String = null,
+    filter: Option[String] = None,
+    iterations: Int = 3,
+    cache_tables: Boolean = false,
+    baseline: Option[Long] = None)
+
 case class LineOrder (
  lo_orderkey : Int,
  lo_linenumber : Int,
@@ -78,71 +85,104 @@ case class Ddate (
  d_holidayfl : Int,
  d_weekdayfl : Int
  )
-
+case class BenchmarkResult(query Int:time_in_ms: Long )
 object SSB {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("SSB")
-    val sc = new SparkContext(conf)
-    val sqlContext : org.apache.spark.sql.SQLContext = new org.apache.spark.sql.SQLContext(sc)
 
-    import sqlContext.implicits._
+    val parser = new scopt.OptionParser[RunConfig]("spark-sql-perf") {
+      head("SSB BenchMark on Spark", "0.2.0")
+      opt[String]('d', "ssb_data").action { (x, c) => c.copy(ssb_path = x) }
+        .text("path to the tbl files")
+        .required()
+      opt[String]('f', "filter")
+        .action((x, c) => c.copy(filter = Some(x)))
+        .text("a filter on the name of the queries to run")
+      opt[Int]('i', "iterations")
+        .action((x, c) => c.copy(iterations = x))
+        .text("the number of iterations to run")
+      opt[Long]('c', "compare")
+          .action((x, c) => c.copy(baseline = Some(x)))
+          .text("the timestamp of the baseline experiment to compare with")
+      opt[Boolean]('h', "cache")
+          .action((x, c) => c.copy(cache_tables = x))
+          .text("Cache tables")
+      help("help")
+        .text("prints this usage text")
+        }
 
-    var table_names = Array("lineorder", "part", "supplier", "customer", "date")
-    // for (i <- 0 until table_names.length) {
-    //     val lineorder =  sc.textFile(f.getPath()).map(_.split("\\|")).map(p => LineOrder(p(0).trim.toInt, p(1).trim.toInt,p(2).trim.toInt,p(3).trim.toInt,p(4).trim.toInt,p(5).trim.toInt,p(6),p(7),p(8).trim.toInt,p(9).trim.toInt,p(10).trim.toInt,p(11).trim.toInt,p(12).trim.toInt,p(13).trim.toInt,p(14).trim.toInt,p(15).trim.toInt,p(16))).toDF()
-    // }
+        parser.parse(args, RunConfig()) match {
+          case Some(config) =>
+            run(config)
+          case None =>
+            System.exit(1)
+        }
 
-    val ssb_path = "/home/marc/code/benchmarks/tpch+ssb/ssb_tbl/sf1/"
-    val f: java.io.File = new File(ssb_path ,"lineorder.tbl")
-    val lineorder =  sc.textFile(f.getPath()).map(_.split("\\|")).map(p => LineOrder(p(0).trim.toInt, p(1).trim.toInt,p(2).trim.toInt,p(3).trim.toInt,p(4).trim.toInt,p(5).trim.toInt,p(6),p(7),p(8).trim.toInt,p(9).trim.toInt,p(10).trim.toInt,p(11).trim.toInt,p(12).trim.toInt,p(13).trim.toInt,p(14).trim.toInt,p(15).trim.toInt,p(16))).toDF()
-    val customer = sc.textFile((new File(ssb_path, "customer.tbl")).getPath()).map(_.split("\\|")).map(p => Customer(p(0).trim.toInt, p(1),p(2),p(3),p(4),p(5),p(6),p(7))).toDF()
+    def run(config: RunConfig) {
+        val conf = new SparkConf().setAppName("SSB")
+        val sc = new SparkContext(conf)
+        val sqlContext : org.apache.spark.sql.SQLContext = new org.apache.spark.sql.SQLContext(sc)
 
-    val supplier = sc.textFile((new File(ssb_path,"supplier.tbl")).getPath()).map(_.split("\\|")).map(p => Supplier(p(0).trim.toInt, p(1),p(2),p(3),p(4),p(5),p(6))).toDF()
+        import sqlContext.implicits._
+        // setting shuffle partitions to 1
+        sqlContext.setConf("spark.sql.shuffle.partitions", "1")
+        var table_names = Array("lineorder", "part", "supplier", "customer", "date")
+        // for (i <- 0 until table_names.length) {
+        //     val lineorder =  sc.textFile(f.getPath()).map(_.split("\\|")).map(p => LineOrder(p(0).trim.toInt, p(1).trim.toInt,p(2).trim.toInt,p(3).trim.toInt,p(4).trim.toInt,p(5).trim.toInt,p(6),p(7),p(8).trim.toInt,p(9).trim.toInt,p(10).trim.toInt,p(11).trim.toInt,p(12).trim.toInt,p(13).trim.toInt,p(14).trim.toInt,p(15).trim.toInt,p(16))).toDF()
+        // }
 
-    val date = sc.textFile((new File(ssb_path,"date.tbl")).getPath()).map(_.split("\\|")).map(p => Ddate(p(0).trim.toInt, p(1),p(2),p(3),p(4).trim.toInt,p(5).trim.toInt,p(6),p(7).trim.toInt,p(8).trim.toInt,p(9).trim.toInt,p(10).trim.toInt,p(11).trim.toInt,p(12),p(13).trim.toInt,p(14).trim.toInt,p(15).trim.toInt,p(16).trim.toInt)).toDF()
+        val ssb_path = config.ssb_path
+        val f: java.io.File = new File(ssb_path ,"lineorder.tbl")
+        val lineorder =  sc.textFile(f.getPath()).map(_.split("\\|")).map(p => LineOrder(p(0).trim.toInt, p(1).trim.toInt,p(2).trim.toInt,p(3).trim.toInt,p(4).trim.toInt,p(5).trim.toInt,p(6),p(7),p(8).trim.toInt,p(9).trim.toInt,p(10).trim.toInt,p(11).trim.toInt,p(12).trim.toInt,p(13).trim.toInt,p(14).trim.toInt,p(15).trim.toInt,p(16))).toDF()
+        val customer = sc.textFile((new File(ssb_path, "customer.tbl")).getPath()).map(_.split("\\|")).map(p => Customer(p(0).trim.toInt, p(1),p(2),p(3),p(4),p(5),p(6),p(7))).toDF()
 
-    val part = sc.textFile((new File(ssb_path,"part.tbl")).getPath()).map(_.split("\\|")).map(p => Part(p(0).trim.toInt, p(1),p(2),p(3),p(4),p(5),p(6),p(7).trim.toInt,p(8))).toDF()
+        val supplier = sc.textFile((new File(ssb_path,"supplier.tbl")).getPath()).map(_.split("\\|")).map(p => Supplier(p(0).trim.toInt, p(1),p(2),p(3),p(4),p(5),p(6))).toDF()
 
-    lineorder.registerTempTable("lineorder")
-    customer.registerTempTable("customer")
-    supplier.registerTempTable("supplier")
-    date.registerTempTable("date")
-    part.registerTempTable("part")
-    for (i <- 0 until table_names.length) {
-      sqlContext.cacheTable(table_names(i))
-    }
+        val date = sc.textFile((new File(ssb_path,"date.tbl")).getPath()).map(_.split("\\|")).map(p => Ddate(p(0).trim.toInt, p(1),p(2),p(3),p(4).trim.toInt,p(5).trim.toInt,p(6),p(7).trim.toInt,p(8).trim.toInt,p(9).trim.toInt,p(10).trim.toInt,p(11).trim.toInt,p(12),p(13).trim.toInt,p(14).trim.toInt,p(15).trim.toInt,p(16).trim.toInt)).toDF()
 
-    val Q1="select sum(lo_extendedprice*lo_discount) as revenue from lineorder, date where lo_orderdate = d_datekey and d_year = 1993 and lo_discount between 1 and 3 and lo_quantity < 25"
-    val Q2="select sum(lo_extendedprice*lo_discount) as revenue from lineorder, date where lo_orderdate = d_datekey and d_yearmonthnum = 199401 and lo_discount between 4 and 6 and lo_quantity between 26 and 35"
-    val Q3="select sum(lo_extendedprice*lo_discount) as revenue from lineorder, date where lo_orderdate = d_datekey and d_weeknuminyear = 6 and d_year = 1994 and lo_discount between 5 and 7 and lo_quantity between 36 and 40"
-    val Q4="select sum(lo_revenue), d_year, p_brand1 from lineorder, date, part, supplier where lo_orderdate = d_datekey and lo_partkey = p_partkey and lo_suppkey = s_suppkey and p_category = 'MFGR#12' and s_region = 'AMERICA' group by d_year, p_brand1 order by d_year, p_brand1"
-    val Q5="select sum(lo_revenue), d_year, p_brand1 from lineorder, date, part, supplier where lo_orderdate = d_datekey and lo_partkey = p_partkey and lo_suppkey = s_suppkey and p_brand1 between 'MFGR#2221' and 'MFGR#2228' and s_region = 'ASIA' group by d_year, p_brand1 order by d_year, p_brand1"
-    val Q6="select sum(lo_revenue), d_year, p_brand1 from lineorder, date, part, supplier where lo_orderdate = d_datekey and lo_partkey = p_partkey and lo_suppkey = s_suppkey and p_brand1 = 'MFGR#2221' and s_region = 'EUROPE' group by d_year, p_brand1 order by d_year, p_brand1"
-    val Q7="select c_nation, s_nation, d_year, sum(lo_revenue) as revenue from customer, lineorder, supplier, date where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_orderdate = d_datekey and c_region = 'ASIA' and s_region = 'ASIA' and d_year >= 1992 and d_year <= 1997 group by c_nation, s_nation, d_year order by d_year asc, revenue desc"
-    val Q8="select c_city, s_city, d_year, sum(lo_revenue) as revenue from customer, lineorder, supplier, date where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_orderdate = d_datekey and c_nation = 'UNITED STATES' and s_nation = 'UNITED STATES' and d_year >= 1992 and d_year <= 1997 group by c_city, s_city, d_year order by d_year asc, revenue desc"
-    val Q9="select c_city, s_city, d_year, sum(lo_revenue) as revenue from customer, lineorder, supplier, date where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_orderdate = d_datekey and (c_city='UNITED KI1' or c_city='UNITED KI5') and (s_city='UNITED KI1' or s_city='UNITED KI5') and d_year >= 1992 and d_year <= 1997 group by c_city, s_city, d_year order by d_year asc, revenue desc"
-    val Q10="select c_city, s_city, d_year, sum(lo_revenue) as revenue from customer, lineorder, supplier, date where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_orderdate = d_datekey and (c_city='UNITED KI1' or c_city='UNITED KI5') and (s_city='UNITED KI1' or s_city='UNITED KI5') and d_yearmonth = 'Dec1997' group by c_city, s_city, d_year order by d_year asc, revenue desc"
-    val Q11="select d_year, c_nation, sum(lo_revenue-lo_supplycost) as profit1 from date, customer, supplier, part, lineorder where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_partkey = p_partkey and lo_orderdate = d_datekey and c_region = 'AMERICA' and s_region = 'AMERICA' and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2') group by d_year, c_nation order by d_year, c_nation"
-    val Q12="select d_year, s_nation, p_category, sum(lo_revenue-lo_supplycost) as profit1 from date, customer, supplier, part, lineorder where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_partkey = p_partkey and lo_orderdate = d_datekey and c_region = 'AMERICA' and s_region = 'AMERICA' and (d_year = 1997 or d_year = 1998) and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2') group by d_year, s_nation, p_category order by d_year, s_nation, p_category"
-    val Q13="select d_year, s_city, p_brand1, sum(lo_revenue-lo_supplycost) as profit1 from date, customer, supplier, part, lineorder where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_partkey = p_partkey and lo_orderdate = d_datekey and c_region = 'AMERICA' and s_nation = 'UNITED STATES' and (d_year = 1997 or d_year = 1998) and p_category = 'MFGR#14' group by d_year, s_city, p_brand1 order by d_year, s_city, p_brand1"
+        val part = sc.textFile((new File(ssb_path,"part.tbl")).getPath()).map(_.split("\\|")).map(p => Part(p(0).trim.toInt, p(1),p(2),p(3),p(4),p(5),p(6),p(7).trim.toInt,p(8))).toDF()
 
-    var query_array = Array(Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12, Q13)
-    var times_array:Array[Long] = new Array[Long](query_array.length)
+        lineorder.registerTempTable("lineorder")
+        customer.registerTempTable("customer")
+        supplier.registerTempTable("supplier")
+        date.registerTempTable("date")
+        part.registerTempTable("part")
+        if (config.cache_tables) {
+            for (i <- 0 until table_names.length) {
+              sqlContext.cacheTable(table_names(i))
+            }
+         }
+        val Q1="select sum(lo_extendedprice*lo_discount) as revenue from lineorder, date where lo_orderdate = d_datekey and d_year = 1993 and lo_discount between 1 and 3 and lo_quantity < 25"
+        val Q2="select sum(lo_extendedprice*lo_discount) as revenue from lineorder, date where lo_orderdate = d_datekey and d_yearmonthnum = 199401 and lo_discount between 4 and 6 and lo_quantity between 26 and 35"
+        val Q3="select sum(lo_extendedprice*lo_discount) as revenue from lineorder, date where lo_orderdate = d_datekey and d_weeknuminyear = 6 and d_year = 1994 and lo_discount between 5 and 7 and lo_quantity between 36 and 40"
+        val Q4="select sum(lo_revenue), d_year, p_brand1 from lineorder, date, part, supplier where lo_orderdate = d_datekey and lo_partkey = p_partkey and lo_suppkey = s_suppkey and p_category = 'MFGR#12' and s_region = 'AMERICA' group by d_year, p_brand1 order by d_year, p_brand1"
+        val Q5="select sum(lo_revenue), d_year, p_brand1 from lineorder, date, part, supplier where lo_orderdate = d_datekey and lo_partkey = p_partkey and lo_suppkey = s_suppkey and p_brand1 between 'MFGR#2221' and 'MFGR#2228' and s_region = 'ASIA' group by d_year, p_brand1 order by d_year, p_brand1"
+        val Q6="select sum(lo_revenue), d_year, p_brand1 from lineorder, date, part, supplier where lo_orderdate = d_datekey and lo_partkey = p_partkey and lo_suppkey = s_suppkey and p_brand1 = 'MFGR#2221' and s_region = 'EUROPE' group by d_year, p_brand1 order by d_year, p_brand1"
+        val Q7="select c_nation, s_nation, d_year, sum(lo_revenue) as revenue from customer, lineorder, supplier, date where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_orderdate = d_datekey and c_region = 'ASIA' and s_region = 'ASIA' and d_year >= 1992 and d_year <= 1997 group by c_nation, s_nation, d_year order by d_year asc, revenue desc"
+        val Q8="select c_city, s_city, d_year, sum(lo_revenue) as revenue from customer, lineorder, supplier, date where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_orderdate = d_datekey and c_nation = 'UNITED STATES' and s_nation = 'UNITED STATES' and d_year >= 1992 and d_year <= 1997 group by c_city, s_city, d_year order by d_year asc, revenue desc"
+        val Q9="select c_city, s_city, d_year, sum(lo_revenue) as revenue from customer, lineorder, supplier, date where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_orderdate = d_datekey and (c_city='UNITED KI1' or c_city='UNITED KI5') and (s_city='UNITED KI1' or s_city='UNITED KI5') and d_year >= 1992 and d_year <= 1997 group by c_city, s_city, d_year order by d_year asc, revenue desc"
+        val Q10="select c_city, s_city, d_year, sum(lo_revenue) as revenue from customer, lineorder, supplier, date where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_orderdate = d_datekey and (c_city='UNITED KI1' or c_city='UNITED KI5') and (s_city='UNITED KI1' or s_city='UNITED KI5') and d_yearmonth = 'Dec1997' group by c_city, s_city, d_year order by d_year asc, revenue desc"
+        val Q11="select d_year, c_nation, sum(lo_revenue-lo_supplycost) as profit1 from date, customer, supplier, part, lineorder where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_partkey = p_partkey and lo_orderdate = d_datekey and c_region = 'AMERICA' and s_region = 'AMERICA' and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2') group by d_year, c_nation order by d_year, c_nation"
+        val Q12="select d_year, s_nation, p_category, sum(lo_revenue-lo_supplycost) as profit1 from date, customer, supplier, part, lineorder where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_partkey = p_partkey and lo_orderdate = d_datekey and c_region = 'AMERICA' and s_region = 'AMERICA' and (d_year = 1997 or d_year = 1998) and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2') group by d_year, s_nation, p_category order by d_year, s_nation, p_category"
+        val Q13="select d_year, s_city, p_brand1, sum(lo_revenue-lo_supplycost) as profit1 from date, customer, supplier, part, lineorder where lo_custkey = c_custkey and lo_suppkey = s_suppkey and lo_partkey = p_partkey and lo_orderdate = d_datekey and c_region = 'AMERICA' and s_nation = 'UNITED STATES' and (d_year = 1997 or d_year = 1998) and p_category = 'MFGR#14' group by d_year, s_city, p_brand1 order by d_year, s_city, p_brand1"
 
-    val q_rdd = sqlContext.sql(query_array(0))
-    q_rdd.collect()
+        var query_array = Array(Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12, Q13)
+        var times_array:Array[Long] = new Array[Long](query_array.length)
 
-    for (i <- 0 until query_array.length) {
-      val start = System.currentTimeMillis()
-      val q_rdd = sqlContext.sql(query_array(i))
-      q_rdd.collect()
-      val end = System.currentTimeMillis()
-      times_array(i) = end - start
-      System.out.println("Completed " + (i + 1) + " in " + times_array(i))
-    }
+        val q_rdd = sqlContext.sql(query_array(0))
+        q_rdd.collect()
 
-    for (i <- 0 until query_array.length) {
-      System.out.println("" + (i + 1) + "," + times_array(i))
-    }
-  }
+        for (i <- 0 until query_array.length) {
+          val start = System.currentTimeMillis()
+          val q_rdd = sqlContext.sql(query_array(i))
+          q_rdd.collect()
+          val end = System.currentTimeMillis()
+          times_array(i) = end - start
+          System.out.println("Completed " + (i + 1) + " in " + times_array(i))
+        }
+
+        for (i <- 0 until query_array.length) {
+          System.out.println("" + (i + 1) + "," + times_array(i))
+        }
+      }
+   }
 }
--- ssb.sbt
+++ ssb.sbt
@@ -1,9 +1,13 @@
-name := "SSB Queries"
+name := "SSBQueries"
 
 version := "1.0"
 
 scalaVersion := "2.10.4"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.1"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.1" % "provided"
 
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.6.1"   
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.6.1" % "provided"
+
+libraryDependencies += "com.github.scopt" %% "scopt" % "3.4.0"
+
+resolvers += Resolver.sonatypeRepo("public")
